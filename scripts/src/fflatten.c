66dc754b7c7e92c679e35a97d8b44b0de591bbf9
Minato <mnyoshie@gmx.com>
2024-06-05 19:02:02 +0800

    update copyright

--- scripts/src/fflatten.c
+++ scripts/src/fflatten.c
@@ -1,6 +1,6 @@
 /* fflaten.c - fast flatten
  *
- * Copyright (C) 2024 Minato Yoshie
+ * Copyright (C) 2024 Minato Yoshie & Al-buharie Amjari
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
